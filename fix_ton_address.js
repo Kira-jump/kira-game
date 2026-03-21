3c92531024fe143619d9102a5fbd1d08f14e1f7a
Mamadou Saliou <yagam900@gmail.com>
2026-03-21 02:46:12 +0000

    Fix adresse TON wallet

--- fix_ton_address.js
+++ fix_ton_address.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 let html = fs.readFileSync('public/index.html', 'utf8');
 
 html = html.replace(
-    'address: \'UQBsGhMhvLmHCwvLvRBVyXpMGAIbLFrjNHQgC7_1bZVKmKrK\'',
-    'address: \'UQDy7R-iFzXr7CkNf6YWUAlHI1QfscSw7MzNzNRea5CZncnp\''
+    'address: \'UQDGKus1j0cjtOTy-FeqvGDpqfohTLlL82L03_GXDpl6tZSE\'',
+    'address: \'UQDGKus1j0cjtOTy-FeqvGDpqfohTLlL82L03_GXDpl6tZSE\''
 );
 
 fs.writeFileSync('public/index.html', html);
